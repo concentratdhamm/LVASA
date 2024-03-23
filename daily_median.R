d6e29a215310a61b91612503dbdb63d89df855ae
concentratdhamm <hameed_adelabu@hotmail.com>
2024-03-23 11:42:31 +0100

    23/03/2024 11:41am

--- daily_median.R
+++ daily_median.R
@@ -34,10 +34,39 @@ for (j in 1:nrow(trend_df_lga)) {
   }
 }
 
-trend_df_lga["overall_mean"] <- NA
+# trend_df_lga["overall_mean"] <- NA
 
 # for (i in nrow(trend_df_lga)){
 #   trend_df_lga[i, ncol(trend_df_lga)] <- sapply(trend_df_lga, mean)
 # }
 
 write_xlsx(trend_df_lga, "daily_trend_lga.xlsx")
+
+EA_LGA <- About_the_household %>%
+  dplyr::select(record_id, lga_name)
+
+# Return all records and all variables
+House_Mapping_raw <-
+  REDCapR::redcap_report(
+    redcap_uri = "https://live.cctris.org/redcap_v13.7.1/API/",
+    token      = "02814B207A231775DB2EDE53D3F9E014",
+    report_id  = 48,
+    raw_or_label = "raw"
+  )$data
+
+House_Mapping_raw <- House_Mapping_raw %>%
+  dplyr::select(record_id, enumeration_code)
+
+House_Mapping_raw <- rename(House_Mapping_raw, enumeration_code_raw = enumeration_code)
+
+EA_LGA <- EA_LGA %>%
+  left_join(House_Mapping_raw, by = 'record_id')
+
+EA_LGA_with_Codes <- EA_LGA %>%
+  dplyr::filter(!is.na(enumeration_code_raw))
+
+EA_LGA_with_Codes <- EA_LGA_with_Codes %>%
+  group_by(enumeration_code_raw) %>%
+  dplyr::filter(row_number()==1)
+
+write_xlsx(EA_LGA_with_Codes, "EA_LGA_with_Codes.xlsx")

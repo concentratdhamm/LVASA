270b6493c793468ec442e0a72739a744ace0c701
concentratdhamm <hameed_adelabu@hotmail.com>
2024-04-14 15:06:53 +0100

    14/04/2024 15:06PM

--- rev_geocoding.R
+++ rev_geocoding.R
@@ -3,13 +3,34 @@ library(tidygeocoder)
 library(sf)
 library(mapview)
 library(readxl)
+library(writexl)
 
-House_Mapping <- read_xlsx("House_Mapping_v2.xlsx", guess_max = 20000)
+# Return all records and all variables
+Geolocation <-
+  REDCapR::redcap_report(
+    redcap_uri = "https://live.cctris.org/redcap_v13.7.1/API/",
+    token      = "02814B207A231775DB2EDE53D3F9E014",
+    report_id  = 41,
+    raw_or_label = "label"
+  )$data
 
-geo_reverse_tbl <- House_Mapping %>%
+drop_columns <- c('redcap_event_name')
+Geolocation <- Geolocation %>%
+  select(-one_of(drop_columns))
+
+Geolocation <- Geolocation %>%
+  add_column(Unique_location = 
+               (Geolocation$geolocation_of_house_lat + Geolocation$geolocation_of_house_longi)
+             , .after = "geolocation_of_house_longi")
+
+Unique_Geo <- Geolocation %>%
+  group_by(Unique_location) %>%
+  dplyr::filter(row_number()==1)
+
+geo_reverse_tbl <- Unique_Geo %>%
   
   # Pretend we don't have the address...
-  select(-address) %>%
+  # select(-address) %>%
   
   # Go from Lat/Lon to Address
   tidygeocoder::reverse_geocode(
@@ -17,3 +38,5 @@ geo_reverse_tbl <- House_Mapping %>%
     long   = geolocation_of_house_longi,
     method = "osm"
   )
+
+write_xlsx(geo_reverse_tbl, "Geolocation_osm.xlsx")
